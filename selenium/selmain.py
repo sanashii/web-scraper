6b0bb9c4b68b5c6c153a26c908b92a43f2a6faa2
Andrea Baulita <andybaulita@gmail.com>
2024-01-12 14:59:05 +0800

    added selenium prac

--- selenium/selmain.py
+++ selenium/selmain.py
@@ -1,19 +1,26 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys #this module allows us to have access to keyboard keys such as enter, esc, and etc
 import time
 
-PATH = r"C:\Program Files (x86)\chromedriver.exe"
-
-# Create ChromeOptions instance
+os.environ['PATH'] += r";C:/SeleniumDrivers"
 options = webdriver.ChromeOptions()
+driver = webdriver.Chrome()
 
-# Specify the executable path using the add_argument method
-options.add_argument("executable_path=" + PATH)
-
-# Pass options to the Chrome driver
-driver = webdriver.Chrome(options=options)
-
-driver.get("https://www.reddit.com/r/SHFiguarts/comments/107sx8d/is_this_gohan_worth_getting/")
+driver.get("https://www.reddit.com/?rdt=37323")
+driver.implicitly_wait(30) # similar to time.sleep(30) but this method is used for times when an element is already found and will then move on so we dont have to explicitly wait (time.sleep)
 print(driver.title)
 
-time.sleep(20)
+search = driver.find_element(By.ID, "login-button")
+search.click()
+# search.send_keys("jujutsu kaisen") # what we want to search for
+# search.send_keys(Keys.RETURN) # return is basically enter so we're clicking enter after searching
+
+# print(driver.page_source)
+
+# while True:
+#     pass
+time.sleep(30)
+
 driver.quit()
