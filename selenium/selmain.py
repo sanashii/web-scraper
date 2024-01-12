b91257d861cee23c75955507a1d45d989a3e65df
Andrea Baulita <andybaulita@gmail.com>
2024-01-12 16:49:06 +0800

    updates with implicit & explicit waiting

--- selenium/selmain.py
+++ selenium/selmain.py
@@ -2,6 +2,8 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys #this module allows us to have access to keyboard keys such as enter, esc, and etc
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 os.environ['PATH'] += r";C:/SeleniumDrivers"
@@ -9,7 +11,8 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome()
 
 driver.get("https://www.reddit.com/?rdt=37323")
-driver.implicitly_wait(30) # similar to time.sleep(30) but this method is used for times when an element is already found and will then move on so we dont have to explicitly wait (time.sleep)
+driver.implicitly_wait(30) # similar to time.sleep(30) but this method is used for times when an element is already found and will then move on
+# ^^ more efficient than time.sleep() method
 print(driver.title)
 
 search = driver.find_element(By.ID, "login-button")
@@ -19,8 +22,12 @@ search.click()
 
 # print(driver.page_source)
 
-# while True:
-#     pass
-time.sleep(30)
+WebDriverWait(driver, 30).until( # explicit wait which wil wait until a condition is true
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'mt-md text-14'),
+        ' New to Reddit? '
+    )
+)
+
 
 driver.quit()
